192ec21d7c74bd3525ec3e3b27d368c32dee03f1
yusuke-k-03 <sinkuku01@gmail.com>
2021-01-20 12:33:15 +0900

    テーブル作成

---
+++ db/migrate/20210120030959_create_dictionaries.rb
@@ -0,0 +1,11 @@
+class CreateDictionaries < ActiveRecord::Migration[6.0]
+  def change
+    create_table :dictionaries do |t|
+      t.string :title, null: false
+      t.text :text, null: false
+      t.integer :category_id, null: false
+
+      t.timestamps
+    end
+  end
+end
