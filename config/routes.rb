50090eef6e842d5da1423e4aec3fecf7fdcde0a9
yusuke-k-03 <sinkuku01@gmail.com>
2021-01-28 15:58:52 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'dictionaries/index'
   root to: "dictionaries#index"
+  get 'dictionaries/index'
+  
   resources :dictionaries do
     collection do
       get 'search'
